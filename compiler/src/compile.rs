6e5b1f3d9c3a4f3ba4c2815e765e9b46db1ac5a9
LAKE <lake.pnt@gmail.com>
2026-05-15 22:21:26 +0900

    コードの種類分析

--- compiler/src/compile.rs
+++ compiler/src/compile.rs
@@ -4,4 +4,5 @@ mod typescript;
 
 pub fn execute(encoded_data:String) {
     let code_array = array::convert(encoded_data);
+    categorize::execute(code_array);
 }
--- compiler/src/compile/categorize.rs
+++ compiler/src/compile/categorize.rs
@@ -1,5 +1,33 @@
-pub fn execute(code_array:Vec<String>) {
-    for code_line in code_array {
-        
+use std::{collections::HashMap, hash::Hash};
+
+#[derive(Hash, Eq, PartialEq, Debug, Clone, Copy)]
+pub enum CodeType {
+    None,
+    Define,
+    If,
+    For
+}
+
+pub fn execute(code_array: Vec<String>) {
+    let mut code_hash: HashMap<(usize, CodeType), String> = HashMap::new();
+
+    for (index, code_line) in code_array.iter().enumerate() {
+        let key = if code_line.contains("!") {
+            (index, CodeType::Define)
+        } else if code_line.contains("if") {
+            (index, CodeType::If)
+        } else {
+            (index, CodeType::None)
+        };
+
+        code_hash.insert(key, code_line.to_string());
+    }
+
+    // --- index でソートして視覚化 ---
+    let mut items: Vec<_> = code_hash.iter().collect();
+    items.sort_by_key(|((index, _), _)| *index);
+
+    for ((index, codetype), value) in items {
+        println!("{} | {:?} | {}", index, codetype, value);
     }
 }
